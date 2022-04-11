3a06af12dfdc3537d1d88b4f8fb0844695869c4d
Maxim Partin <max.physmath.05@gmail.com>
2022-04-11 11:15:08 +0300

    add solution task 58 Length of Last Word

---
+++ 58.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        if (s.empty()) {
+            return 0;
+        }
+
+        int i = s.size() - 1;
+
+        while ( i >= 0 && s[i] == ' ') {
+            --i;
+        }
+
+        int length = 0;
+
+        while (i >= 0 && s[i] != ' ') {
+            ++length;
+            --i;
+        }
+
+        return length;
+    }
+};
+
+int main() {
+    return 0;
+}
