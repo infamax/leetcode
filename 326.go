383c55a4a91c1841f5566124f852a49980f4105f
Maxim Partin <86999066+infamax@users.noreply.github.com>
2022-06-10 17:18:36 +0300

    add 326. Power of Three

---
+++ 326.go
@@ -0,0 +1,18 @@
+func isPowerOfThree(n int) bool {
+	if n <= 0 {
+		return false
+	}
+
+	if n == 1 {
+		return true
+	}
+	
+	for n > 1 {
+		if n % 3 != 0 {
+			return false
+		}
+		n /= 3
+	}
+	
+	return true
+}
